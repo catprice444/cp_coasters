263a1639d5b2c40736e76c2adc0301d2aae1b807
Catherine Price <catprice444@gmail.com>
2020-06-20 21:25:30 +0000

    save 6/20/2020

--- lib/cp_coasters/coasters.rb
+++ lib/cp_coasters/coasters.rb
@@ -47,26 +47,4 @@ class CpCoasters::Coasters
     second_coaster
   end
 
-  # def self.indiv_ride_list
-  #   self.scrape_ride_name
-  # end
-  #
-  # def self.scrape_ride_name
-  #   ride_names = []
-  #   ride_names << self.ride_information
-  #
-  #   ride_names
-  # end
-  #
-  # def self.ride_information
-  #   ride = self.new
-  #
-  #   ride.name = "Rollercoaster"
-  #   ride.description = "I'm fun!"
-  #   ride.min_height = "48 inches"
-  #   ride.speed = "FAST"
-  #   ride.duration = "2 minutes"
-  #
-  #   ride
-  # end
 end
