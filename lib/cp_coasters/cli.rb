e583874d0e6f8080dcf9049420245fc347b57a44
Catherine Price <catprice444@gmail.com>
2020-06-20 19:33:58 +0000

    cli test

--- lib/cp_coasters/cli.rb
+++ lib/cp_coasters/cli.rb
@@ -8,35 +8,35 @@ class CpCoasters::CLI
   def rollercoasters
     puts ""
     puts "Which Rollercoaster would you like to learn more about? Enter a number between 1-18."
-    input = gets.strip.to_i
-      if input == "1"
-        puts "1. Blue Streak - The Must Ride Classic - 48 inches"
-      elsif input == "2"
-        puts "2. Mine Ride - Mine for Thrills - 48 inches"
-      else
-        puts "Sorry I don't understand"
-      end
-
-    # coaster = CpCoasters::Coasters.find(input.to_i)
-
-    # print_rollercoaster(coaster)
+    input = gets.strip
+    if input == "1"
+      puts "The First coaster"
+    elsif input == "2"
+      puts "The Second coaster"
+    else
+      puts "Sorry I don't understand"
+    end
+    # ride = CpCoasters::Coasters.find(input.to_i)
+    #
+    # print_rollercoaster(ride)
 
     puts "Are you finished looking at rollercoasters? Enter 'yes' or 'no'."
     input = gets.strip.downcase
-      if input == "yes"
+      if input == "no"
         rollercoasters
-      elsif input == "no"
+      elsif input == "yes"
         puts "Enjoy your stay at Cedar Park!!"
         exit
       else
         puts "Sorry I don't understand"
         rollercoasters
       end
+
+    def print_rollercoaster(ride)
+
+    end
   end
 
 
 
 end
-
-# 1. Blue Streak - The Must Ride Classic - 48 inches
-# 2. Mine Ride - Mine for Thrills - 48 inches
--- lib/cp_coasters/coasters.rb
+++ lib/cp_coasters/coasters.rb
@@ -1,7 +1,8 @@
 class CpCoasters::Coasters
+  attr_accessor :name, :description, :min_height, :speed, :duration
 
   @@all = []
 
   def self.find(input)
-  end 
+  end
 end
