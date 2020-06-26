33838c28a0aedc76387a70195ab68365cd17d6df
Catherine Price <catprice444@gmail.com>
2020-06-26 23:21:26 +0000

    scraper

--- lib/cp_coasters/scraper.rb
+++ lib/cp_coasters/scraper.rb
@@ -1,6 +1,8 @@
 class CpCoasters::Scraper
 
   def all_rollercoasters
+    doc = Nokogiri::HTML(open("https://www.cedarpoint.com/play/rides-coasters"))
+    binding.pry
   end
 
   def indiv_rollercoaster
