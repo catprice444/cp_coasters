0e845acd2f6c94f5c8b989b4c0f43e2f82e4e9e7
Catherine Price <catprice444@gmail.com>
2020-06-26 23:13:03 +0000

    require nokogiri

--- lib/cp_coasters.rb
+++ lib/cp_coasters.rb
@@ -1,13 +1,8 @@
-# require 'open-uri'
-# require 'nokogiri'
-# require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
 
 require_relative "./cp_coasters/version"
 require_relative "./cp_coasters/cli"
 require_relative "./cp_coasters/coasters"
-
-# module CpCoasters
-#   # class Error < StandardError; end
-#   # Your code goes here...
-# end
