f2e49a17c5cb721ddcfe521b474be673e990ede1
Catherine Price <catprice444@gmail.com>
2020-06-27 13:50:13 +0000

    including scraper object

--- lib/cp_coasters.rb
+++ lib/cp_coasters.rb
@@ -1,4 +1,5 @@
-
+require 'nokogiri'
+require 'open-uri'
 
 require 'pry'
 
@@ -6,3 +7,4 @@ require 'pry'
 require_relative "./cp_coasters/version"
 require_relative "./cp_coasters/cli"
 require_relative "./cp_coasters/coasters"
+require_relative "./cp_coasters/scraper"
